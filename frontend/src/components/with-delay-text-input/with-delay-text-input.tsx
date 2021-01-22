9965cb61dec5c2afb0a779fa233dba504ac4fd54
Jorge Requena <requenajorge1995@gmail.com>
2021-01-22 19:55:10 -0400

    Delayed text input last timeout is handled with useRef

--- frontend/src/components/with-delay-text-input/with-delay-text-input.tsx
+++ frontend/src/components/with-delay-text-input/with-delay-text-input.tsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import TextInput from '../text-input/text-input';
 
 function WithDelayTextInput(props: Props) {
   const { name, delayedHandleChange, delay = 1000 } = props;
-
   const [value, setValue] = useState('');
-  const [currentTimeout, setCurrentTimeout] = useState({} as NodeJS.Timeout);
 
-  return <TextInput name={name} value={value} handleChange={handleChange} />;
+  const timeoutRef = useRef({} as NodeJS.Timeout);
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const input = event.target.value;
-    setValue(input);
-    clearTimeout(currentTimeout);
-    setCurrentTimeout(setTimeout(() => delayedHandleChange(input), delay));
-  }
+  return (
+    <TextInput
+      name={name}
+      value={value}
+      handleChange={(event) => {
+        const text = event.target.value;
+        setValue(text);
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => delayedHandleChange(text), delay);
+      }}
+    />
+  );
 }
 
 type Props = {
